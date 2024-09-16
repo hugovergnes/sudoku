5e9c47ee56d5fcc414ee6ca675e8dd74287863d7
hugovergnes <hugo.vergnes@gmail.com>
2024-09-15 20:43:51 -0700

    Added medium sudoku

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sudoku.solve import naive_solve
 
 if __name__ == '__main__':
     table = Table()
-    table.load_from_csv('./sudoku_files/easy/sudoku_2.csv') # One is broken
+    table.load_from_csv('./sudoku_files/medium/sudoku_1.csv')
     naive_solve(table)
     res = table.get_table_data()
     print(res)
