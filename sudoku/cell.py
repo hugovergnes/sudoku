2552851cdf62ce8e1189a0c9e1f3d0e3717283c3
hugovergnes <hugo.vergnes@gmail.com>
2024-09-17 17:13:12 -0700

    Clean up cell class

--- sudoku/cell.py
+++ sudoku/cell.py
@@ -3,7 +3,10 @@ class Cell:
         """_summary_
 
         Args:
+            pos (Tuple): Position of the Cell in the sudoku table.
             value (int, optional): Value in the cell. Defaults to 0 if the cell in unknown yet.
+            candidate_values (set): Possible values in that cell. If the cell is already set to
+            non-0. Then It is empty.
 
         Raises:
             RuntimeError: _description_
@@ -14,31 +17,32 @@ class Cell:
 
     def __str__(self):
         return str(self.value)
-    
+
     def __eq__(self, other):
         if isinstance(other, int):
             return self.value == other
         return self.value == other.value and self.value == other.value
-    
+
     def is_known(self):
         return self.value is not None
 
     def get_value(self):
         return self.value
-    
+
     def set_value(self, value):
-        # if self.value != 0:
-        #     raise RuntimeError(f'Trying to set a value of a known cell')
         self.value = value
-    
+        self.candidate_values = None
+
     def get_candidate_values(self):
         return self.candidate_values
 
     def get_prohibited_values(self):
-        prohibited_values = sorted(set([i for i in range(1, 10)]) - self.candidate_values)
+        prohibited_values = sorted(
+            set([i for i in range(1, 10)]) - self.candidate_values
+        )
         return prohibited_values
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unknown_cell = Cell()
     known_cell = Cell(value=9)
